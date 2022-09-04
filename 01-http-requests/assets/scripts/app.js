1455610eb21954c1819a56a573002e79bc5d07d9
Valentina Lullo <valentina.lullo@reedonline.co.uk>
2022-09-04 18:35:44 +0100

    Adding DELETE post functionality

--- 01-http-requests/assets/scripts/app.js
+++ 01-http-requests/assets/scripts/app.js
@@ -2,6 +2,7 @@ const listElement = document.querySelector(".posts");
 const postTemplate = document.getElementById("single-post");
 const form = document.querySelector("#new-post form");
 const fetchButton = document.querySelector("#available-posts button");
+const postList = document.querySelector("ul");
 
 function sendHttpRequest(method, url, data) {
   const promise = new Promise((resolve, reject) => {
@@ -29,6 +30,7 @@ async function fetchPosts() {
     const postEl = document.importNode(postTemplate.content, true);
     postEl.querySelector("h2").textContent = post.title.toUpperCase();
     postEl.querySelector("p").textContent = post.body;
+    postEl.querySelector("li").id = post.id;
     listElement.append(postEl);
   }
 }
@@ -45,10 +47,21 @@ async function createPost(title, content) {
 }
 
 fetchButton.addEventListener("click", fetchPosts);
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const enteredTitle = event.currentTarget.querySelector("#title").value;
   const enteredContent = event.currentTarget.querySelector("#content").value;
 
   createPost(enteredTitle, enteredContent);
+});
+
+postList.addEventListener("click", (event) => {
+  if (event.target.tagName === "BUTTON") {
+    const postId = event.target.closest("li").id;
+    sendHttpRequest(
+      "DELETE",
+      `https://jsonplaceholder.typicode.com/posts/${postId}`
+    );
+  }
 });
